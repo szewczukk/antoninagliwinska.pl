7df09a6cc3e5c5baf2ac87f9ed043382f06e44f3
szewczukk <dmitro.szewczuk@gmail.com>
2023-09-08 19:44:47 +0200

    Configure prettier & clean the project

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -4,6 +4,15 @@ const config = {
     tabWidth: 4,
     semi: false,
     singleQuote: true,
+    plugins: ['prettier-plugin-astro'],
+    overrides: [
+        {
+            files: '*.astro',
+            options: {
+                parser: 'astro',
+            },
+        },
+    ],
 }
 
 export default config
